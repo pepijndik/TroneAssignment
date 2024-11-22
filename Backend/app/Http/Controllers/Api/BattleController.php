3f2128201401d185add434c3a8d41a01a534cf3d
pdik <pepijn@pdik.nl>
2024-11-22 10:52:49 +0100

    Initial commit

---
+++ Backend/app/Http/Controllers/Api/BattleController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+
+class BattleController extends Controller{
+    public function index()
+    {
+        //
+    }
+}
